30e43ceba3946b54ed6f847f269189eec7c145c8
lovmint <sna135246@gmail.com>
2023-09-16 16:54:40 +0900

    전체 기능 완성

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -29,15 +29,32 @@ public class WordManager {
     }
 
     public void start() {
+
+        wordCRUD.loadFile();
         while (true) {
             int menu = selectMenu();
             if(menu == 0) break;
             if(menu == 4){
-                wordCRUD.addWord();
+                wordCRUD.addItem();
             }
             else if(menu ==1){
                 wordCRUD.listAll();
 
+            }
+            else if(menu ==2){
+                wordCRUD.searchLevel();
+            }
+            else if(menu ==3){
+                wordCRUD.searchWord();
+            }
+            else if(menu == 5){ // update
+                wordCRUD.updateItem();
+            }
+            else if(menu ==6){ //delete
+                wordCRUD.deleteItem();
+            }
+            else if(menu ==7){
+                wordCRUD.saveFile();
             }
         }
     }
